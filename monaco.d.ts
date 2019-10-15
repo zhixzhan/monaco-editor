86bef95df50ead64b33654704ff2f5aca8bc062a
zhixzhan <49866537+zhixzhan@users.noreply.github.com>
2019-10-15 18:13:55 +0800

    LGbundle no eval

--- monaco.d.ts
+++ monaco.d.ts
@@ -1,6 +1,6 @@
 /*!-----------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
- * Type definitions for monaco-editor v0.17.4
+ * Type definitions for monaco-editor v0.17.5
  * Released under the MIT license
 *-----------------------------------------------------------*/
 /*---------------------------------------------------------------------------------------------
