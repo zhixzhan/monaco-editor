0f055209e1fa07e1f262c4ed2d85f2fd84fb08ca
zhixzhan <49866537+zhixzhan@users.noreply.github.com>
2019-10-22 17:22:05 +0800

    upgrade

--- monaco.d.ts
+++ monaco.d.ts
@@ -1,6 +1,6 @@
 /*!-----------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
- * Type definitions for monaco-editor v0.17.5
+ * Type definitions for monaco-editor v0.17.6
  * Released under the MIT license
 *-----------------------------------------------------------*/
 /*---------------------------------------------------------------------------------------------
