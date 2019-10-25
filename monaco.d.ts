f9da0a01b4828557eea61029c12579c712b53b69
zhixzhan <49866537+zhixzhan@users.noreply.github.com>
2019-10-25 10:33:01 +0800

    update lg theme

--- monaco.d.ts
+++ monaco.d.ts
@@ -1,6 +1,6 @@
 /*!-----------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
- * Type definitions for monaco-editor v0.18.2
+ * Type definitions for monaco-editor v0.18.3
  * Released under the MIT license
 *-----------------------------------------------------------*/
 /*---------------------------------------------------------------------------------------------
